957141df539d18b67e53316aa887b764acb87aae
Linda <lindakjellberg7@gmail.com>
2025-11-03 13:51:57 +0100

    Add regular function called subtraction to illustrate difference from arrow function

--- TS-Intro/src/main.ts
+++ TS-Intro/src/main.ts
@@ -1,2 +1,12 @@
 import './style.css'
+import { add, subtract } from './utils';
 
+const firstname: string = "Mumin";
+
+console.log(firstname);
+
+const a = add(5, 6);
+console.log(a);
+
+const s = subtract(30, 7);
+console.log(s);
