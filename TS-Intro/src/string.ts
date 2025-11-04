728561083b0fdd6bacc184238899d6d79c0dc94b
Linda <lindakjellberg7@gmail.com>
2025-11-04 16:11:45 +0100

    Add new file for string assignment operators

---
+++ TS-Intro/src/string.ts
@@ -0,0 +1,6 @@
+/*================================== String Assignment Operators ==================================*/
+
+// Arrow function to perform Simple Assignment string (=) operation:
+
+// Arrow function to perform Addition Assignment string (+=) concatenation:
+
