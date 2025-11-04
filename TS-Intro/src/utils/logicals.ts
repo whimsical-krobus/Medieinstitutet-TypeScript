9d264acb461c1cb88c52daee0b9ec05538cfe538
Linda <lindakjellberg7@gmail.com>
2025-11-04 17:01:06 +0100

    Add logical and operation as an arrow function

--- TS-Intro/src/utils/logicals.ts
+++ TS-Intro/src/utils/logicals.ts
@@ -1,6 +1,9 @@
 /*================================== Logical Operators ==================================*/
 
 // Arrow function to perform Logical AND (&&) operation:
+export const logicalAnd = (x: number, y: number, a: number, b: number): boolean => {
+    return x < y && a > b;
+} 
 
 // Arrow function to perform Logical OR (||) operation:
 
