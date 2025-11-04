5fca9cbadc33bf84c9174e1907358b29d1fc1520
Linda <lindakjellberg7@gmail.com>
2025-11-04 10:06:53 +0100

    Remove variable name and change y value

--- TS-Intro/src/main.ts
+++ TS-Intro/src/main.ts
@@ -1,11 +1,8 @@
 import './style.css'
 import { add, subtract } from './utils';
 
-const firstname: string = "Mumin";
 
-console.log(firstname);
-
-const a = add(5, 6);
+const a = add(5, 10);
 console.log(a);
 
 const s = subtract(30, 7);
