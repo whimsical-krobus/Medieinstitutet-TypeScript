9fb313089748e2149aacfe1f5a7a26bf8402f561
Linda <lindakjellberg7@gmail.com>
2025-11-04 11:54:22 +0100

    Add comments for comming operations

--- TS-Intro/src/utils.ts
+++ TS-Intro/src/utils.ts
@@ -1,46 +1,85 @@
 /*================================== Arithmetic Operators ==================================*/
 
-// Arrow function to perform Addition calculation:
+// Arrow function to perform Addition (+) calculation:
 export const add = (x: number, y: number): number => {
     return x + y;
 }
 
-// Regular function to perform Subtraction calculation:
+// Regular function to perform Subtraction (-) calculation:
 export function subtract(x: number, y: number): number {
   return x - y;
 }
 
-// Arrow function to perform Multiplication calculation:
+// Arrow function to perform Multiplication (*) calculation:
 export const multiply = (x: number, y: number): number => {
   return x * y;
 }
 
-// Arrow function to perform Division calculation:
+// Arrow function to perform Division (/) calculation:
 export const divide = (x: number, y: number): number => {
   return x / y;
 }
 
-// Arrow function to perform Modulus calculation:
+// Arrow function to perform Modulus (%) calculation:
 export const modulo = (x: number, y: number): number => {
   return x % y;
 }
 
-// Arrow function to perform Increment calculation:
+// Arrow function to perform Increment (++) calculation:
 
-// Arrow function to perform Decrement calculation:
+// Arrow function to perform Decrement (--) calculation:
 
 /*================================== Comparison Operators ==================================*/
 
-// Arrow function to perform greater than calculation:
+// Arrow function to perform equal to (==) calculation:
+
+// Arrow function to perform equal value and equal type (===) calculation: 
+
+// Arrow function to perform not equal to (!=) calculation: 
+
+// Arrow function to perform not equal value or not equal type (!==) calculation: 
+
+// Arrow function to perform greater than (>) calculation:
 export const greater = (x: number, y: number): boolean => {
   return x > y;
 }
 
-// Arrow function to perform less than calculation:
+// Arrow function to perform less than (<) calculation:
 export const less = (x: number, y: number): boolean => {
   return x < y;
 }
 
+// Arrow function to perform greater than or equal to (>=) calculation:
+
+// Arrow function to perform less than or equal to (<=) calculation:
 
 /*================================== Assignment Operators ==================================*/
+
+// Arrow function to perform Addition Assignment (+=) calculation:
+
+// Arrow function to perform Subtraction Assignment (-=) calculation:
+
+// Arrow function to perform Multiplication Assignment (*=) calculation:
+
+// Arrow function to perform Exponentiation Assignment (**=) calculation:
+
+// Arrow function to perform Division Assignment (/=) calculation:
+
+// Arrow function to perform Remainder Assignment (%=) calculation:
+
+// Arrow function to perform Simple Assignment (=) calculation:
+
+/*================================== String Assignment Operators ==================================*/
+
+// Arrow function to perform Simple Assignment string (=) operation:
+
+// Arrow function to perform Addition Assignment string (+=) concatenation:
+
+
 /*================================== Logical Operators ==================================*/
+
+// Arrow function to perform Logical AND (&&) operation:
+
+// Arrow function to perform Logical OR (||) operation:
+
+// Arrow function to perform Logical NOT (!) operation:
