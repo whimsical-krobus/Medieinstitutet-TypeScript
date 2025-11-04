a28653bbc932c7a8b15edc9c8d7fb945bee67dfd
Linda <lindakjellberg7@gmail.com>
2025-11-04 10:30:46 +0100

    Add comments briefly describing each function

--- TS-Intro/src/utils.ts
+++ TS-Intro/src/utils.ts
@@ -1,19 +1,24 @@
+// Arrow function to perform addition calculation:
 export const add = (x: number, y: number): number => {
     return x + y;
 }
 
+// Regular function to perform subtraction calculation:
 export function subtract(x: number, y: number): number {
   return x - y;
 }
 
+// Arrow function to perform multiplication calculation:
 export const multiply = (x: number, y: number): number => {
   return x * y;
 }
 
+// Arrow function to perform division calculation:
 export const division = (x: number, y: number): number => {
   return x / y;
 }
 
+// Arrow function to perform modulus calculation:
 export const modulo = (x: number, y: number): number => {
   return x % y;
 }
