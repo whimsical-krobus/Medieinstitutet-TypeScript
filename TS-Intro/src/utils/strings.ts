e2ae204f4d9de74449c1a5e15bcc1aaefbe8e048
Linda <lindakjellberg7@gmail.com>
2025-11-04 16:45:50 +0100

    Add string assignment as an arrow function

--- TS-Intro/src/utils/strings.ts
+++ TS-Intro/src/utils/strings.ts
@@ -1,6 +1,9 @@
 /*================================== String Assignment Operators ==================================*/
 
 // Arrow function to perform Simple Assignment string (=) operation:
+export const stringAssign = (x: string, y: string): string => {
+    return x = y;
+}
 
 // Arrow function to perform Addition Assignment string (+=) concatenation:
 
