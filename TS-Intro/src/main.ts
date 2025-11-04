16aa05feffb5b0fc1243d4b817e5cf9ab4798d71
Linda <lindakjellberg7@gmail.com>
2025-11-04 10:36:39 +0100

    Add comments briefly describing each variable using a function

--- TS-Intro/src/main.ts
+++ TS-Intro/src/main.ts
@@ -1,18 +1,22 @@
 import './style.css'
 import { add, division, modulo, multiply, subtract } from './utils';
 
-
+// Create a variable that uses add with arguments for x and y and display result in terminal:
 const a = add(5, 10);
 console.log(a);
 
+// Create a variable that uses subtract with arguments for x and y and display result in terminal:
 const s = subtract(30, 7);
 console.log(s);
 
+// Create a variable that uses multiply with arguments for x and y and display result in terminal:
 const m = multiply(8, 5);
 console.log(m);
 
+// Create a variable that uses division with arguments for x and y and display result in terminal:
 const d = division(12, 3);
 console.log(d);
 
+// Create a variable that uses modulo with arguments for x and y (y must be always be 2 for mod to work, otherwise it will act as division) and display result in terminal:
 const mod = modulo(37, 2);
 console.log(mod);
